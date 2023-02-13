6a0acec68765f9d65a09a23fd9a8afe41ce70c6f
RDI <dkdlsekdls@naver.com>
2023-02-13 19:00:32 +0900

    feat/Routing

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,3 +1,10 @@
+import Navber from "../../componenets/NavNar";
+
 export default function Potato() {
-  return "about us";
+  return (
+    <div>
+      <Navber />
+      <h1>About</h1>
+    </div>
+  );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,10 @@
-import { useState } from "react";
+import Navber from "../../componenets/NavNar";
 
 export default function Home() {
-  const [counter, setCounter] = useState(0);
   return (
     <div>
-      <h1>Hello {counter}</h1>
-      <button onClick={() => setCounter((prev) => prev + 1)}> + </button>
+      <Navber />
+      <h1>Hello </h1>
     </div>
   );
 }
