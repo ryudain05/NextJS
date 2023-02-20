1c3f24e980f7f23d18b4d9b9225aad9c95c87e95
RDI <dkdlsekdls@naver.com>
2023-02-20 17:58:12 +0900

    feat/Server Side Rendering

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,10 @@
 import Seo from "/components/Seo";
-import { useState, useEffect } from "react";
 
-export default function Home() {
-  const [movies, setMovies] = useState([]);
-  useEffect(() => {
-    (async () => {
-      const { results } = await (await fetch("/api/movies")).json();
-      setMovies(results);
-    })();
-  }, []);
-
+export default function Home({ results }) {
   return (
     <div className="container">
       <Seo title="Home" />
-      {!movies && <h4>Loading...</h4>}
-      {movies.map((movie) => (
+      {results.map((movie) => (
         <div className="movie" key={movie.id}>
           <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
           <h4>{movie.original_title}</h4>
@@ -43,4 +33,16 @@ export default function Home() {
       `}</style>
     </div>
   );
+}
+
+//server에서만 실행됨
+export async function getServerSideProps() {
+  const { results } = await (
+    await fetch("http://localhost:3000/api/movies")
+  ).json();
+  return {
+    props: {
+      results,
+    },
+  };
 }
